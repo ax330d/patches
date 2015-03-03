45ab434a0606e87130937728ee164fafe70ecb7e
ag-work-pc <ax330d@gmail.com>
2015-03-03 14:12:04 +0100

    Update to version 0.0.2
    Updated preferences are now unique, sorted alphabetically, added more
    prefs.

--- patch-moz-prefs.py
+++ patch-moz-prefs.py
@@ -3,13 +3,15 @@
 #
 # by ax330d
 #
-# v 0.1     09-11-2014
+# v 0.0.1     09-11-2014
+# v 0.0.2     03-03-2015
 
-__version__ = '0.1'
-
+import re
 import os
 import argparse
 import ConfigParser
+
+__version__ = '0.0.2'
 
 
 class AttributeDict(dict):
@@ -22,8 +24,10 @@ class AttributeDict(dict):
 
 
 class MozPrefsPatcher(object):
+    '''Mozilla Firefox preferences patcher.'''
     def __init__(self, path):
         self.path = path
+        self.prefs = None
 
     def run(self):
         '''Main function - parse config file, patch prefs.js.'''
@@ -36,8 +40,8 @@ class MozPrefsPatcher(object):
             if item == 'General':
                 continue
             path_to_file = os.path.sep.join([self.path,
-                                            config.get(item, 'path'),
-                                            'prefs.js'])
+                                             config.get(item, 'path'),
+                                             'prefs.js'])
             self.patch(path_to_file)
         return
 
@@ -47,21 +51,91 @@ class MozPrefsPatcher(object):
         print "Patching {}...".format(path_to_file)
 
         prefsjs_data = self._get_prefjs_data()
-        with open(path_to_file, 'a') as fh:
-            fh.write(prefsjs_data)
+
+        with open(path_to_file) as handle:
+            prefs_default = handle.readlines()
+
+        buff = ""
+        cache = {}
+        for preference in prefs_default:
+            matches = re.match(r'user_pref\("(.*?)", (.*?)\);', preference)
+            if not matches:
+                buff += preference
+                continue
+            key = matches.group(1)
+            val = matches.group(2)
+            if key in cache:
+                continue
+            if key in self.prefs:
+                continue
+            cache[key] = val
+
+        for key in sorted(cache):
+            val = cache[key]
+            buff += 'user_pref("{}", {});\n'.format(key, val)
+
+        buff += "\n{}".format(prefsjs_data)
+
+        with open(path_to_file, 'w') as handle:
+            handle.write(buff)
         return
 
-    @classmethod
-    def _get_prefjs_data(cls):
+    def _get_prefjs_data(self):
         '''Create Firefox-format preference data.'''
 
         prefs = {}
+
+        # This has been crashing for me too often
         prefs['javascript.options.ion'] = 'false'
+
+        # Disable e10s
         prefs['browser.tabs.remote.autostart'] = 'false'
         prefs['browser.tabs.remote.autostart.1'] = 'false'
 
+        # Disable crash reporting and things
+        prefs['datareporting.healthreport.service.firstRun'] = 'false'
+        prefs['datareporting.healthreport.service.enabled'] = 'false'
+        prefs['datareporting.healthreport.uploadEnabled'] = 'false'
+        prefs['dom.ipc.plugins.flash.subprocess.crashreporter.enabled'] = 'false'
+        prefs['dom.ipc.plugins.reportCrashURL'] = 'false'
+
+        # Safe fuzzing continuation
+        prefs['browser.sessionstore.max_resumed_crashes'] = -1
+        prefs['browser.sessionstore.resume_from_crash'] = 'false'
+        prefs['toolkit.startup.max_resumed_crashes'] = -1
+
+        # Enable some features
+        prefs['webgl.disabled'] = 'false'
+        prefs['canvas.filters.enabled'] = 'true'
+        prefs['canvas.hitregions.enabled'] = 'true'
+        prefs['dom.imagecapture.enabled'] = 'true'
+        prefs['dom.indexedDB.experimental'] = 'true'
+        prefs['dom.mapped_arraybuffer.enabled'] = 'true'
+        prefs['dom.serviceWorkers.enabled'] = 'true'
+        prefs['dom.w3c_pointer_events.enabled'] = 'true'
+        prefs['dom.webcomponents.enabled'] = 'true'
+        prefs['editor.use_css'] = 'true'
+        prefs['layout.css.grid.enabled'] = 'true'
+        prefs['layout.css.outline-style-auto.enabled'] = 'true'
+        prefs['layout.css.overflow-clip-box.enabled'] = 'true'
+        prefs['layout.css.text-align-true-value.enabled'] = 'true'
+        prefs['layout.css.touch_action.enabled'] = 'true'
+        prefs['layout.css.vertical-text.enabled'] = 'true'
+        prefs['layout.event-regions.enabled'] = 'true'
+        prefs['media.audio_data.enabled'] = 'true'
+        prefs['media.mediasource.webm.enabled'] = 'true'
+        prefs['media.peerconnection.ice.loopback'] = 'true'
+        prefs['media.peerconnection.identity.enabled'] = 'true'
+        prefs['media.peerconnection.video.h264_enabled'] = 'true'
+        prefs['media.track.enabled'] = 'true'
+        prefs['media.webspeech.recognition.enable'] = 'true'
+        prefs['media.webspeech.synth.enabled'] = 'true'
+        prefs['media.webvtt.regions.enabled'] = 'true'
+        prefs['webgl.force-enabled'] = 'true'
+
+        self.prefs = prefs
         data = ''
-        for key in prefs:
+        for key in sorted(prefs):
             val = prefs[key]
             data += 'user_pref("{}", {});\n'.format(key, val)
         return data
